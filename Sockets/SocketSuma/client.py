d36480014b0d1978d4d6c800d913e7bfc4db093a
VanessaPenagos <angie-19-v@utp.edu.co>
2017-02-08 22:15:02 -0500

    servidor suma dos numeros del cliente

--- Sockets/SocketSuma/client.py
+++ Sockets/SocketSuma/client.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python
 import socket
 
-Dato1 = "35"
-Dato2 = "50"
+Datos = ["35" , "50" ]
 
 s = socket.socket()
 s.connect(('localhost', 9999))
-s.send(Dato1)
+s.send(Datos[0])
 dato = s.recv(1024)
-s.send(Dato2)
+s.send(Datos[1])
 dato = s.recv(1024)
 s.close()
-print "Se enviaron los numeros : "
-print Dato1 , Dato2
-print "La suma de los datos es : ", dato
+print "Se enviaron los numeros :", Datos[0] , Datos[1]
+print "La suma de los datos es :", dato
