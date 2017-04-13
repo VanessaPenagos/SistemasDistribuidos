7c88d970c4dd2cfd003d089961cba369a027041e
Daniela Zuluaga <daniela-zuluaga@utp.edu.co>
2017-04-13 18:34:41 -0500

    Registro de archivos completo

--- Proyecto1/Cliente.py
+++ Proyecto1/Cliente.py
@@ -2,6 +2,18 @@ import xmlrpclib
 import random
 import time
 
-while True:
-	s = xmlrpclib.ServerProxy('http://localhost:8006')
-	print "Digite el archivo a  Solicitado ", resource
+
+##################### CLIENTE ###########################
+
+IP = '127.0.0.1'
+PUERTO = 8007
+Archivos = ["hola.txt", "vane.txt", "diego.txt"]
+
+server = xmlrpclib.ServerProxy('http://localhost:8006')
+registroCliente = server.RegistrarCliente(IP, PUERTO)
+print registroCliente
+if registroCliente != "Cliente ya registrado":
+	for i in Archivos:
+		registroArchivo = server.RegistrarArchivos(i, IP, PUERTO)
+		time.sleep(1)
+		print registroArchivo
--- Proyecto1/Servidor DNS.py
+++ Proyecto1/Servidor DNS.py
@@ -1,19 +1,20 @@
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
-
-class RequestHandler(SimpleXMLRPCRequestHandler):
-    rpc_paths = ('/RPC2',)
+import random
 
 IP = '127.0.0.1'
 PORT = 8006
 BUFER_SIZE = 20
 
+class RequestHandler(SimpleXMLRPCRequestHandler):
+    rpc_paths = ('/RPC2',)
 
 class Archivo:
-	def __init__(self, nombre, direcciona, permiso):
+	def __init__(self, nombre, ip, puerto, permiso):
 		self.nombre = nombre
-		self.permiso = permiso
-		self.direcciona = direcciona
+		self.permiso = permiso # 0-> escritura , 1-> lectura, 2-> ambos
+		self.ipcliente = ip
+		self.puertocliente = puerto
 
 class Cliente:
 	def __init__(self, ip, puerto) :
@@ -21,19 +22,59 @@ class Cliente:
 		self.direccionip = ip
 		self.puerto = puerto
 
-	def EliminarArchivo (self):
-		pass
+	def AgregarArchivo (self, archivo):
+		self.archivos.append(archivo)
 
-# Create server
+class ArchivosCompartidos:
+	def __init__ (self, ip, puerto, nombre):
+		self.ip = ip
+		self.puerto = puerto
+		self.nombre = nombre
+
 server = SimpleXMLRPCServer((IP,PORT),
                             requestHandler=RequestHandler)
 server.register_introspection_functions()
 
 class MyFuncs:
 	def __init__(self):
-		self.recursos = []
-	def RegistrarRecursos (self):
+		self.clientes = []
+		self.archivosCompartidos = []
 
+	def RegistrarCliente (self, ip, puerto):
+		for cliente in self.clientes:
+			if cliente.direccionip == ip and cliente.puerto == puerto:
+				return "Cliente ya registrado"
+		cliente = Cliente(ip, puerto)
+		self.clientes.append(cliente)
+		return "Cliente Registrado"
+
+	def RegistrarArchivos (self, nombre, ip, puerto):
+		archivo = Archivo(nombre, ip, puerto, 2)
+		for a in self.archivosCompartidos:
+			if a.nombre == nombre:
+				return "Archivo ya registrado"
+		for cliente in self.clientes:
+			if cliente.direccionip == ip and cliente.puerto == puerto:
+					cliente.AgregarArchivo(archivo)
+		archivoCompartido = ArchivosCompartidos(ip, puerto, nombre)
+		self.archivosCompartidos.append(archivoCompartido)
+		return "Archivo "+ nombre +" registrado"
+
+	def ListarArchivos(self):
+		pass
+		return 0
+
+	def BuscarArchivo (self, nombre):
+		pass
+		return 0 
+
+	def GenerarPermisos(self):
+		pass
+		return 0 
+
+	def EliminarArchivos(self):
+		pass
+		return 0
 
 server.register_instance(MyFuncs())
 
