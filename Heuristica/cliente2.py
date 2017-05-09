3a4b90d6dc23cf7279fc6c3aa50a1a7fcce94314
utp <utp@utp>
2017-05-09 10:03:05 -0500

    asigna procesador segun la carga de la maquina

---
+++ Heuristica/cliente2.py
@@ -0,0 +1,41 @@
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
+from datetime import datetime
+import xmlrpclib
+import threading
+import time
+import sys
+import random
+
+
+IP = '127.0.0.1'
+PORT = 8007
+BUFER_SIZE = 20
+
+class RequestHandler(SimpleXMLRPCRequestHandler):
+    rpc_paths = ('/RPC2',)
+
+s = SimpleXMLRPCServer((IP,PORT),
+                            requestHandler=RequestHandler)
+s.register_introspection_functions()
+
+class MyFuncs:
+    self.carga = random.randint(0,100)
+
+s.register_instance(MyFuncs())
+t = threading.Thread(target=s.serve_forever)
+
+portlist = [8005,8006,8007]
+t.start()
+n=0
+while True :
+    while n<3:
+        time.sleep(3)
+        try:
+        	s1 = xmlrpclib.ServerProxy("http://127.0.0.1:"+str(portlist[n]))
+       	except:
+       		pass
+
+        n = n+1
+    n = 0
+sys.exit()
